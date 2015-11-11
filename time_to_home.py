a710956d8ef1a527e49b8ee013c39498562bdb3d
easauceda <easauceda@gmail.com>
2015-11-10 21:26:44 -0800

    Refactored text message

--- time_to_home.py
+++ time_to_home.py
@@ -7,17 +7,17 @@ USERNAME = ""
 PW = ""
 TO = ""
 FROM = ""
-URL = """https://maps.googleapis.com/maps/api/directions/json?origin=Venice,
-+CA&destination=Glendale,+CA
+URL = """https://maps.googleapis.com/maps/api/directions/json?origin=Venice,+CA&destination=Glendale,+CA
 &departure_time=now&key=""" + API_KEY
 
 r = requests.get(URL)
 parsed_r = json.loads(r.text)
 
 travel_time = parsed_r["routes"][0]["legs"][0]["duration_in_traffic"]["text"]
-message = "Going home will take " + travel_time
+message = "It will take " + travel_time + " to get home."
+
 server = smtplib.SMTP("smtp.gmail.com", 587)
 
 server.starttls()
 server.login(USERNAME, PW)
-server.sendmail(FROM, TO, message)
+server.sendmail(FROM, TO,  message)
